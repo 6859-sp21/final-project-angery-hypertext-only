5cd651b87a485eeebe699952a3a30bf7671f0253
KiiKang <wk1kang@gmail.com>
2021-05-19 18:41:53 -0400

    update hypertext

--- hypertext/sketch.js
+++ hypertext/sketch.js
@@ -2,6 +2,8 @@ const frameRate = 60;
 let phrases;
 let startTime;
 
+let quotes = [];
+
 function startTimer(delay) {
   startTime = new Date();
 };
@@ -20,6 +22,29 @@ function loadJSON(callback) {
     xobj.send(null);
 }
 
+let quotes = [];
+let nucleiList = ["family", "parents", "mom", "mother", "father", "kids", "States", "America", "China", "Spanish", "Venezuelan", "Venezuela", "connection", ""]
+
+function main() {
+ loadJSON(function(response) {
+    let phrases = JSON.parse(response);
+    let quote;
+    phrases.forEach(phrase => {
+      let nuclei = [];
+      phrases[i]["timestamp"].forEach(item => {
+        if (nucleiList.includes(item["word"])) {
+          nuclei.push(item["word"]);
+        }
+      });
+      //Quote(id, nuclei, speaker, fullText, timestamps)
+      quote = new Quote(phrase["id"], nuclei, speaker, phrase["full_text"], timestamp)
+      quotes.push(quote);
+    });
+ });
+ animateMedia(27);
+}
+
+
 var Char = function(container, char) {
   var span = document.createElement("span");
   span.setAttribute('data-word', char);
@@ -36,7 +61,6 @@ var Char = function(container, char) {
       this.draw();
   }
 
-
   this.draw = function() {
       var style = "visibility: visible;";
       style += "opacity: " + this.alpha + ";";
@@ -49,20 +73,23 @@ var Char = function(container, char) {
 
 let chars = [];
 
-function animateMedia(id) {
+function animateQuote(quote) {
+  let id, speaker, timestamp;
+  id = quote.id;
+  speaker = quote.speaker;
+  timestamp = quote.timestamp;
 
-  let phrase = phrases.filter(phrase => phrase["id"] === id)[0];
-  console.log(phrase);
   let count = 0, text = "", wordIndex = [], wordLetterCount = 0;
   let quoteContainer = document.getElementById("container-quote");
 
-    if (phrase["speaker"] === "immigrant"){
+  if (speaker === "immigrant"){
     quoteContainer.style.fontFamily = "Newsreader";
   } else {
     quoteContainer.style.fontFamily = "Compressa VF";
   }
-  for (let i = 0; i < phrase["timestamp"].length; i++){
-    _char = new Char(quoteContainer, phrase["timestamp"][i]["word"] + " ");
+
+  for (let i = 0; i < timestamp.length; i++){
+    _char = new Char(quoteContainer, timestamp[i]["word"] + " ");
     chars.push(_char);
   }
 
@@ -85,22 +112,23 @@ function animateMedia(id) {
       videoFrame.style = "background: radial-gradient(rgba(255,255,0,"
        + String(0.4 + Math.sin(timeElapsed*10)/5) + "), rgba(255,255,0,0) 70%);"
 
-      for (let i = 0; i < phrase["timestamp"].length; i++){
-        if (timeElapsed > phrase["timestamp"][i]["startTime"]){ wordIndexNow = i; }
+      for (let i = 0; i < timestamp.length; i++){
+        if (timeElapsed > timestamp[i]["startTime"]){ wordIndexNow = i; }
       }
 
       if (wordIndexPrev !== wordIndexNow) {
-        console.log(String(wordIndexNow) + "th word:" + phrase["timestamp"][wordIndexNow]["word"]);
-        let width = Math.round(10 * (phrase["timestamp"][wordIndexNow]["endTime"] - phrase["timestamp"][wordIndexNow]["startTime"]) / phrase["timestamp"][wordIndexNow]["word"].length) * 100 + 100;
-        let ital = Math.round(10 * (phrase["timestamp"][wordIndexNow]["endTime"] - phrase["timestamp"][wordIndexNow]["startTime"]) / phrase["timestamp"][wordIndexNow]["word"].length);
-        let weight = phrase["timestamp"][wordIndexNow]["weight"] * 200 + 150;
-        let cap = (phrase["timestamp"][wordIndexNow]["weight"] > 3)? true : false;
+        console.log(String(wordIndexNow) + "th word:" + timestamp[wordIndexNow]["word"]);
+        let width = Math.round(10 * (timestamp[wordIndexNow]["endTime"] - timestamp[wordIndexNow]["startTime"]) / timestamp[wordIndexNow]["word"].length) * 100 + 100;
+        let ital = (wordIndexNow !== 0)?
+                    Math.round(10 * (timestamp[wordIndexNow]["startTime"] - timestamp[wordIndexNow - 1]["startTime"]) / timestamp[wordIndexNow]["word"].length) : width;
+        let weight = timestamp[wordIndexNow]["weight"] * 200 + 150;
+        let cap = (timestamp[wordIndexNow]["weight"] > 3)? true : false;
         chars[wordIndexNow].update({"wdth": width, "wght": weight, "alpha": 1, "ital": ital, "cap": cap, "size": weight/2});
 
-        resetIndex = (wordIndexNow < phrase["timestamp"].length - 2)? ((wordIndexNow > 2)? wordIndexNow - 2: 0): wordIndexNow + 1;
+        resetIndex = (wordIndexNow < timestamp.length - 2)? ((wordIndexNow > 2)? wordIndexNow - 2: 0): wordIndexNow + 1;
 
         for (let j = 0; j < resetIndex; j++){
-          if (phrase["speaker"] === "immigrant"){
+          if (speaker === "immigrant"){
             chars[j].update({"wdth": 100, "wght": 250, "alpha": .5, "ital": 1, "cap": false, "size": 50});
           } else {
             chars[j].update({"wdth": 150, "wght": 250, "alpha": .5, "ital": 0, "cap": false, "size": 50});
@@ -108,7 +136,7 @@ function animateMedia(id) {
         }
       }
 
-      if (timeElapsed > phrase["timestamp"][phrase["timestamp"].length - 1]["endTime"] + 0.2 ){
+      if (timeElapsed > timestamp[timestamp.length - 1]["endTime"] + 0.2 ){
         clearInterval(timer);
         videoFrame.style = "radial-gradient(rgba(255,255,0,0.6), rgba(255,255,0,0) 70%);"
         videoFrame.pause();
@@ -118,11 +146,5 @@ function animateMedia(id) {
   }
 }
 
-function main() {
- loadJSON(function(response) {
-    phrases = JSON.parse(response);
-    animateMedia(3);
- });
-}
 
 main();
