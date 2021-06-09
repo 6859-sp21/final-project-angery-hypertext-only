6598fba9a545913703492141336eafb819431508
Megan Prakash <meganprakash@gmail.com>
2021-06-09 17:02:29 -0400

    removed ts.old

--- ts.old/types/visual_typedef.ts
+++
@@ -1,7 +0,0 @@
-
-
-// TODO in here there are collections of p5 graphics objects maybe???
-// so that they can be collected and hidden/shown
-// IDK if this is necessary
-
-// TODO Oh also the variable font code should live here
